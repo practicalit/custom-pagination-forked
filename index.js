f5a64e769cc1d0afef9900231450795602dd6618
Practical IT <code@thepracticalit.com>
2021-06-08 00:02:39 -0400

    Forked pagination with custom spacing

---
+++ index.js
@@ -0,0 +1,45 @@
+//adapted code
+$(function() {
+      (function(name) {
+        var container = $('#pagination-' + name);
+        var sources = function () {
+          var result = [];
+    
+          for (var i = 1; i < 196; i++) {
+            result.push(i);
+          }
+    
+          return result;
+        }();
+    
+        var options = {
+          dataSource: sources,
+          showLastOnEllipsisShow: false,
+          callback: function (response, pagination) {
+            //window.console && console.log(response, pagination);
+    
+            var dataHtml = '<ul>';
+    
+            $.each(response, function (index, item) {
+              dataHtml += '<li>' + item + '</li>';
+            });
+    
+            dataHtml += '</ul>';
+    
+            container.prev().html(dataHtml);
+          }
+        };
+    
+        //$.pagination(container, options);
+    
+        container.addHook('beforeInit', function () {
+          window.console && console.log('beforeInit...');
+        });
+        container.pagination(options);
+    
+        container.addHook('beforePageOnClick', function () {
+          window.console && console.log('beforePageOnClick...');
+          //return false
+        });
+      })('custom');
+     })
